8bffd8887a5aa56c8a74c47eb9c2faf7d6b53d11
MosiurRrahman <mosiurrahman595@gmail.com>
2020-08-31 06:37:15 +0600

    add price,taxt, total count

---
+++ custom.js
@@ -0,0 +1,45 @@
+// handle product
+function handleProductChange(product, isIncrease) {
+    const productCount = getInputValue(product);
+    let productNewCount = productCount;
+    if (isIncrease == true) {
+        productNewCount = productCount + 1;
+    }
+    if (isIncrease == false && productCount > 0) {
+        productNewCount = productCount - 1;
+    }
+    document.getElementById(product + '-count').value = productNewCount;
+    let productTotal = 0;
+    if (product == 'phone') {
+        productTotal = productNewCount * 1219;
+    }
+    if (product == 'case') {
+        productTotal = productNewCount * 59;
+    }
+    document.getElementById(product + '-total').innerText = "$ " + productTotal;
+    calculateTotal();
+}
+
+
+// calculate total
+
+function calculateTotal(params) {
+    const phoneCount = getInputValue('phone');
+    const caseCount = getInputValue('case');
+
+    const totalPrice = phoneCount * 1219 + caseCount * 59;
+    document.getElementById('total-price').innerText = '$' + totalPrice;
+
+    const tax = Math.round(totalPrice * 0.05);
+    document.getElementById('tax-amount').innerText = '$' + tax;
+
+    const grandTotal = totalPrice + tax;
+    document.getElementById('grand-total').innerText = '$' + grandTotal;
+
+}
+// total price
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-count');
+    const productCount = parseInt(productInput.value);
+    return productCount;
+}
